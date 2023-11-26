d82e69601068ad751b5d62f082d306613f094bc6
Joe Wilson <your_email@example.com>
2023-11-25 17:48:48 -0800

    added results

---
+++ slippy/results/plot_trajectory_quaternion.py
@@ -0,0 +1,73 @@
+import bpy
+import numpy as np
+import sys
+from pandas import read_csv
+    
+# Set the path to your CSV file
+csv_file_path = 'data/test.csv'
+
+# Function to read CSV data
+def csv_to_datadict(file_path: str, fps, time_key='time [s]'):
+    df = read_csv(file_path)
+    datadict ={}
+    for key in df:
+        datadict[key] = df[key].to_numpy()
+
+    # Sub sample data
+    dt = datadict[time_key][1] - datadict[time_key][0]
+    samples =  int((1/fps) / dt)
+    for key in datadict:
+        datadict[key] = datadict[key][::samples]
+    return datadict
+
+# Function to set the location and rotation of the object at a specific frame
+def set_object_transform(frame, obj, pos, quat):
+    obj.location = pos
+    obj.rotation_mode = 'QUATERNION'
+    obj.rotation_quaternion = quat
+    obj.keyframe_insert(data_path="location", frame=frame)
+    obj.keyframe_insert(data_path="rotation_quaternion", frame=frame)
+
+# Calcualte fps to view in real time
+fps = int(55)
+bpy.context.scene.render.fps = fps
+
+# Clear existing keyframes
+bpy.ops.anim.keyframe_clear_v3d()
+
+# Init datadict
+datadict = csv_to_datadict(csv_file_path, fps)
+obj = bpy.data.objects['Cube']
+
+for i,_ in enumerate(datadict['time [s]']):
+    frame = i
+    pos = [
+        datadict['pos.x [m]'][i],
+        datadict['pos.y [m]'][i],
+        datadict['pos.z [m]'][i]
+    ]
+    quat = [
+        datadict['quat.b [-]'][i],
+        datadict['quat.c [-]'][i],
+        datadict['quat.d [-]'][i],
+        datadict['quat.a [-]'][i]
+    ]
+
+    # Modify trajectory for the given frame
+    set_object_transform(i, obj, pos, quat)
+
+
+# Set the end frame to match the last frame in the animation
+bpy.context.scene.frame_end = frame
+
+# Set up rendering settings
+# print(f"Total Frames :{frame}")
+# bpy.context.scene.render.image_settings.file_format = 'FFMPEG'
+# bpy.context.scene.render.ffmpeg.format = 'MPEG4'
+# bpy.context.scene.render.ffmpeg.codec = 'H264'
+# bpy.context.scene.render.filepath = 'output_animation.mp4'
+
+# Render animation
+# bpy.ops.render.render(animation=True)
+
+bpy.ops.screen.animation_play()
---
+++ slippy/results/yaml_plot.py
@@ -0,0 +1,316 @@
+import argparse
+import os
+import pandas as pd
+import numpy as np
+from plotly import graph_objects as go
+import sys
+import re
+from yaml import load
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="plot_yaml",
+        description="plotly from a yaml config"
+    )
+    parser.add_argument(
+        "-i","--input",
+        help="File path to your input yaml"
+    )
+    parser.add_argument(
+        "-o","--output",
+        help="File path to save your plot"
+    )
+    parser.add_argument(
+        "-d","--data",
+        help="File path to your data"
+    )
+    return parser.parse_args()
+
+
+def csv_to_datadict(file_path: str):
+    df = pd.read_csv(file_path)
+    datadict ={}
+    for key in df:
+        datadict[key] = df[key].to_numpy()
+    return datadict
+
+
+def plotly_list_from_datadict(
+    datadict: dict[str, np.array],
+    x_key: str,
+    key_list: list[str],
+    fig: go.Figure,
+    mode: str = 'line',
+    title: str = '',
+    yaxis_number = None
+):
+    for key in key_list:
+
+        if yaxis_number is None or yaxis_number == 0:
+            y_str = 'y'
+        else:
+            y_str = f'y{yaxis_number}'
+            print(y_str)
+
+        fig.add_trace(
+            go.Scatter(
+                mode=mode,
+                x=datadict[x_key],
+                y=datadict[key],
+                name=key,
+                yaxis=y_str
+            )
+        )
+        fig.update_layout(
+            title_text=title,
+            xaxis_title=x_key,
+            showlegend=True
+        )
+
+
+def parse_axis(
+    plot_contents: dict[str, str]
+):
+    pattern = r'^axis\d+$'
+    axis_list = []
+
+    for key in plot_contents:
+        if re.match(pattern, key):
+            axis_list.append(plot_contents[key])
+
+    if len(axis_list) > 8:
+        print('    ERROR| Cannot plot more than 8 axis')
+
+    return axis_list
+
+
+def get_axis_properties(
+    axis_properties: dict[str, str],
+    group_name: str,
+    plot_name: str,
+    x_key: str,
+    datadict: dict[str, np.array]
+):
+
+
+    if 'mode' not in axis_properties:
+        mode = 'lines'
+    else:
+        mode = axis_properties['mode']
+
+    if 'y_label' not in axis_properties:
+        y_label = ''
+    else:
+        y_label = axis_properties['y_label']
+
+    if group_name != ('' or ' '):
+        fig_title = f'{group_name}-{plot_name}'
+    else:
+        fig_title = plot_name
+
+
+    if plot_name == 'All':
+        key_list = [key for key in datadict if key != x_key]
+
+    elif 'key_list' not in axis_properties:
+        print(f'    ERROR| "key_list" not in {plot_name}')
+        sys.exit(1)
+
+    else:
+        key_list = axis_properties['key_list']
+
+    return [mode, y_label, fig_title, key_list]
+
+
+def figs_from_yaml(
+    group_name: str,
+    plot_list: list,
+    datadict: dict[str, np.array]
+) -> list[(str, go.Figure)]:
+
+    fig_list = []
+
+
+    for plot_name, plot_contents in plot_list.items():
+        fig = go.Figure()
+
+        x_key = plot_contents['x_key']
+
+        # Errors
+        if x_key not in datadict:
+            print(f'    ERROR| x_key "{x_key}" not in csv file')
+            exit(1)
+
+        axis_list = parse_axis(plot_contents)
+
+        if len(axis_list):
+
+            # Create the figure
+            fig = go.Figure()
+            for i, axis_properties in enumerate(axis_list):
+                mode, y_label, fig_title, key_list = get_axis_properties(
+                    axis_properties,
+                    group_name,
+                    plot_name,
+                    x_key,
+                    datadict
+                )
+
+                axis = go.layout.YAxis(
+                    title=y_label,
+                    anchor="free",
+                    overlaying="y",
+                    autoshift=True
+                )
+
+                # Support up to 8 axis
+                match i:
+                    case 0:
+                        fig.update_layout(
+                            yaxis= dict(title=y_label)
+                        )
+                    case 1:
+                        fig.update_layout(
+                            yaxis2 = axis
+                        )
+                    case 2:
+                        fig.update_layout(
+                            yaxis3 = axis
+                        )
+                    case 3:
+                        fig.update_layout(
+                            yaxis4 = axis
+                        )
+                    case 4:
+                        fig.update_layout(
+                            yaxis5 = axis
+                        )
+                    case 5:
+                        fig.update_layout(
+                            yaxis6 = axis
+                        )
+                    case 6:
+                        fig.update_layout(
+                            yaxis7 = axis
+                        )
+                    case 7:
+                        fig.update_layout(
+                            yaxis8 = axis
+                        )
+
+                plotly_list_from_datadict(
+                    datadict,
+                    x_key,
+                    key_list,
+                    fig,
+                    mode,
+                    fig_title,
+                    yaxis_number=i+1
+                )
+
+            fig_list.append((fig_title, fig))
+
+        else:
+            print(plot_name, axis_list)
+            mode, y_label, fig_title, key_list = get_axis_properties(
+                plot_contents,
+                group_name,
+                plot_name,
+                x_key,
+                datadict
+            )
+
+            # Create the figure
+            fig = go.Figure()
+            plotly_list_from_datadict(
+                datadict,
+                x_key,
+                key_list,
+                fig,
+                mode,
+                fig_title
+            )
+            fig.update_layout(
+                yaxis_title=y_label,
+            )
+            fig_list.append((fig_title, fig))
+    return fig_list
+
+
+def output_fig_list(fig_list: list[(str, go.Figure)], output_path = None):
+    for tuple in fig_list:
+        name, fig = tuple
+
+        if output_path is None:
+            fig.show()
+        else:
+            fig.write_html(os.path.join(output_path, name + '.html'))
+
+
+def main():
+
+    abs_path = os.getcwd()
+
+    args = parse_args()
+
+    if not args.input:
+        print(f"    ERROR| Please use -i for yaml config path")
+        sys.exit(1)
+    yaml_path = os.path.join(abs_path, args.input)
+    yaml_dir = os.path.dirname(yaml_path)
+
+    output_path = None
+    if args.output:
+
+        if args.output == '.' or '' or 'current':
+            output_path = yaml_dir
+        else:
+            output_path = os.join(abs_path, args.output)
+
+    # Parse Yaml
+    yaml = open(yaml_path, 'r')
+    yaml_dict = load(yaml, Loader=Loader)
+
+    # Globals
+    DATA_PATH_KEY = "DataPath"
+    GROUP_NAME_KEY = "GroupName"
+    PLOT_LIST_KEY = "Plots"
+
+    # Check Errors
+    if args.data:
+        data_path = os.path.join(yaml_dir, args.data)
+    elif not DATA_PATH_KEY in yaml_dict:
+        print(f'    ERROR| "{DATA_PATH_KEY}" field not found in yaml'
+           +'\n            or passed as argument with [-d, --data]')
+        exit(1)
+    else:
+        data_path = yaml_dict[DATA_PATH_KEY]
+
+    if not GROUP_NAME_KEY in yaml_dict:
+        group_name = ''
+    else:
+        group_name = yaml_dict[GROUP_NAME_KEY]
+
+    if not PLOT_LIST_KEY in yaml_dict:
+        print(f'    ERROR| "{PLOT_LIST_KEY}" field not found in yaml')
+        exit(1)
+
+    # Parse inputs
+    path = os.path.join(yaml_dir, data_path)
+    datadict = csv_to_datadict(path)
+
+    plot_dict = yaml_dict[PLOT_LIST_KEY]
+
+    # Plot the files
+    fig_list = figs_from_yaml(group_name, plot_dict, datadict)
+
+    # Output
+    output_fig_list(fig_list, output_path)
+
+if __name__ == '__main__':
+    main()
